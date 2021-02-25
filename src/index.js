4aa293e99158bff2dff4a35f3ad1a87e4151bcc4
french-fries-with-pepper <alexey.s.antonov@zoho.com>
2021-02-25 20:17:37 +0300

    added implementation of min, max and avg functions

--- src/index.js
+++ src/index.js
@@ -1,12 +1,32 @@
 
 exports.min = function min (array) {
-  return 0;
+  if(!array||array.length===0) return 0;
+  const n = array.length;
+  let min = array[0];
+  for (let i = 1; i < n; i++) {
+    if(min>array[i]) min=array[i];
+  }
+  return min;
 }
 
 exports.max = function max (array) {
-  return 0;
+  if(!array||array.length===0) return 0;
+  const n = array.length;
+  let max = array[0];
+  for(let i=1; i<n; i++) {
+    if(max<array[i])max=array[i];
+  }
+  return max;
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  if(!array||array.length===0) return 0;
+  const n = array.length;
+  let avg = 0;
+  let sum = 0;
+  for(let i=0; i<n; i++){
+    sum +=array[i];
+  }
+  avg = sum / n;
+  return avg;
 }
